c44ee4711dc73f34a679762f7907e6b3454728a4
Vinay Kharayat <vinaykharayat@gmail.com>
2023-09-01 14:43:48 +0530

    Some fixes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const ethers = require("ethers");
-const ChainId = 5;
-const abi = require("./abi.json");
-const routerAbi = require("./router.json");
-const myTokenAbi = require("./myToken.json");
 require('dotenv').config();
+const ethers = require("ethers");
+const timestamp = require('unix-timestamp');
+timestamp.round = true;
+const pairAbi = require("./artifacts/pairAbi.json");
+const routerAbi = require("./artifacts/router.json");
+const myTokenAbi = require("./artifacts/myToken.json");
+
 const pairAddress = "0xd0b966d297Be4647f8fA84fA7BCa6efcBd665190";
 const myTokenAddress = "0x289541f355Eb0775f6301145e45743289bf06b4F";
 const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-
+const wethAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+const buyerAddress = "0x31b2eeC73618445625D0Cc8e94Fd50Cda1F3E8B4";
 const provider = new ethers.JsonRpcProvider(process.env.INFURA_URL);
 
 // for example if top-level await is not an option
 const myToken = new ethers.Contract(myTokenAddress, myTokenAbi, provider);
-const pair = new ethers.Contract(pairAddress, abi, provider);
+const pair = new ethers.Contract(pairAddress, pairAbi, provider);
 
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const uniswapRouter = new ethers.Contract(uniswapRouterAddress, routerAbi, signer);
@@ -36,9 +39,9 @@ const swapTokens = async () => {
         }
         const tx = await uniswapRouter.swapExactETHForTokens(
             0,
-            ["0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", "0x289541f355Eb0775f6301145e45743289bf06b4F"],
-            "0x31b2eeC73618445625D0Cc8e94Fd50Cda1F3E8B4",
-            1693664088,
+            [wethAddress, myTokenAddress],
+            buyerAddress,
+            timestamp.now("60s"),
             options
         );
         console.log("Tansaction:", tx);
